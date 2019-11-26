0639891099439e58d31aae85e1d85d226b6b60cc
Gnxela <qawswedfrgtt@gmail.com>
2019-11-26 20:04:06 +0000

    Changed input to be more realistic

--- src/me/alexng/gns/Main.java
+++ src/me/alexng/gns/Main.java
@@ -7,8 +7,8 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Script script = new Script(String.join("\n", new String[]{
 				"i = 0",
-				"if (i = 1) {",
-				"	if (i = 0) {",
+				"if (i == 1) {",
+				"	if (i == 0) {",
 				"		i = 1",
 				"	}",
 				"	i = 2",
