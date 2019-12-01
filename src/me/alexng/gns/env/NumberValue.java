0c77cd9478e5753a84c8d55ee3a20bf8d203b6e6
Gnxela <qawswedfrgtt@gmail.com>
2019-12-01 20:33:21 +0000

    Started implementing number values. I'm not a fan of how it's done right now. Having and int and double value is weird, but I also don't want to constantly store a double if we're only using the number as an int. Not sure what to do.

--- src/me/alexng/gns/env/NumberValue.java
+++ src/me/alexng/gns/env/NumberValue.java
@@ -2,13 +2,31 @@ package me.alexng.gns.env;
 
 public class NumberValue extends Value {
 
+	private boolean isInt;
+	private int intValue;
+	private double doubleValue;
+
 	public NumberValue(String value) {
 		super(Type.NUMBER);
+		parseValue(value);
+	}
+
+	private void parseValue(String value) {
+		if (value.contains(".")) {
+			doubleValue = Double.parseDouble(value);
+			isInt = false;
+		} else {
+			intValue = Integer.parseInt(value);
+			isInt = true;
+		}
 	}
 
 	@Override
-	public Integer getValue() {
-		// TODO: Implement number values.
-		return null;
+	public Number getValue() {
+		return isInt ? intValue : doubleValue;
+	}
+
+	public boolean isInt() {
+		return isInt;
 	}
 }
