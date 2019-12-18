23d03a53494fa050d9f6e4a9a9df795a2db2192b
Gnxela <qawswedfrgtt@gmail.com>
2019-12-18 12:59:28 +0000

    Removed nested classes check at runtime. Updated script to show what nested classes might look like. I actually think I might allow this, it's a nice feature to have in a lot of situations. Nested functions I'm not sure sure yet.

--- src/me/alexng/gns/tokens/ClassToken.java
+++ src/me/alexng/gns/tokens/ClassToken.java
@@ -18,10 +18,6 @@ public class ClassToken extends IdentifiedToken {
 
 	@Override
 	public Value execute(Scope scope) throws RuntimeException {
-		if (!scope.isGlobalScope()) {
-			// TODO: Don't want to do this at runtime.
-			throw new RuntimeException(getFileIndex(), "Can't define classes outside of global scope");
-		}
 		scope.addClass(this);
 		return Value.NULL;
 	}
