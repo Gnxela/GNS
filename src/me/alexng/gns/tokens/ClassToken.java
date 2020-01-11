761b6e04abba1372f9a88afe8e03f27ff6eba4b0
Gnxela <qawswedfrgtt@gmail.com>
2020-01-11 12:52:32 +0000

    Added restricted class names.

--- src/me/alexng/gns/tokens/ClassToken.java
+++ src/me/alexng/gns/tokens/ClassToken.java
@@ -1,24 +1,31 @@
 package me.alexng.gns.tokens;
 
 import me.alexng.gns.FileIndex;
+import me.alexng.gns.ParsingException;
 import me.alexng.gns.RuntimeException;
 import me.alexng.gns.env.scope.Scope;
 import me.alexng.gns.env.value.NumberValue;
 import me.alexng.gns.env.value.ObjectValue;
 import me.alexng.gns.env.value.StringValue;
 import me.alexng.gns.env.value.Value;
+import me.alexng.gns.util.ArrayUtil;
 import me.alexng.gns.util.StringUtil;
 
 public class ClassToken extends IdentifiedToken {
 
+	// TODO: When we add modular values, load them here
+	private static final String[] RESTRICTED_NAMES = new String[]{"number", "string", "boolean"};
 	private static final IdentifierToken OBJECT_ID_VARIABLE = new IdentifierToken("objectId", FileIndex.INTERNAL_INDEX);
 	private static final IdentifierToken TYPE_VARIABLE = new IdentifierToken("type", FileIndex.INTERNAL_INDEX);
 	private static final IdentifierToken CONSTRUCTOR_NAME = new IdentifierToken("init", FileIndex.INTERNAL_INDEX);
 
 	private BlockToken block;
 
-	public ClassToken(IdentifierToken identifier, BlockToken block, FileIndex fileIndex) {
+	public ClassToken(IdentifierToken identifier, BlockToken block, FileIndex fileIndex) throws ParsingException {
 		super(identifier, fileIndex);
+		if (ArrayUtil.arrayContains(RESTRICTED_NAMES, identifier.getName())) {
+			throw new ParsingException(this, "Restricted class name");
+		}
 		this.block = block;
 	}
 
