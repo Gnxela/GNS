bc706d39083a8bf0c6e3ced4ff416fd4d6aaf3a5
Gnxela <qawswedfrgtt@gmail.com>
2020-01-31 20:17:49 +0000

    Added JUnit to the project. And so my watch begins.

---
+++ src/test/java/me/alexng/gns/FileIndexTest.java
@@ -0,0 +1,13 @@
+package me.alexng.gns;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FileIndexTest {
+
+	@Test
+	public void testOpenClose() {
+		assertEquals(1, 1);
+	}
+}
