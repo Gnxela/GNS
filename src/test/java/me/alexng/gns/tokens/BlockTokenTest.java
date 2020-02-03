23d5ab416a532893c644bfb269623e438949ecd7
Gnxela <qawswedfrgtt@gmail.com>
2020-02-03 11:03:02 +0000

    Finished tests for BlockToken.

--- src/test/java/me/alexng/gns/tokens/BlockTokenTest.java
+++ src/test/java/me/alexng/gns/tokens/BlockTokenTest.java
@@ -1,13 +1,19 @@
 package me.alexng.gns.tokens;
 
 import me.alexng.gns.FileIndex;
+import me.alexng.gns.ParsingException;
 import me.alexng.gns.RuntimeException;
 import me.alexng.gns.env.scope.Scope;
+import me.alexng.gns.env.value.NumberValue;
+import me.alexng.gns.tokens.operators.AssignToken;
 import me.alexng.gns.util.CountingExecutor;
 import me.alexng.gns.util.ExecutableMockToken;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BlockTokenTest {
 
@@ -23,5 +29,49 @@ public class BlockTokenTest {
 		countingExecutor.assertCount(3);
 	}
 
-	// TODO: Test the scopes can be modified inside a block.
+	@Test
+	public void testExecuteBlockWithScope_modifyScope() throws RuntimeException, ParsingException {
+		IdentifierToken identifier = new IdentifierToken("test", FileIndex.NULL_INDEX);
+		AssignToken assignToken = new AssignToken(FileIndex.NULL_INDEX);
+		assignToken.bind(identifier, new ValueToken(new NumberValue(13), FileIndex.NULL_INDEX));
+		LinkedList<Token> tokens = new LinkedList<>();
+		tokens.add(assignToken);
+		BlockToken blockToken = new BlockToken(tokens, FileIndex.NULL_INDEX);
+		Scope globalScope = Scope.createGlobalScope(null);
+		blockToken.executeBlockWithScope(globalScope);
+		assertEquals(13, globalScope.variableProvider.get(identifier).getJavaValue());
+		assertEquals(13, globalScope.variableProvider.getLocal(identifier).getJavaValue());
+	}
+
+	@Test
+	public void testExecute() throws RuntimeException, ParsingException {
+		CountingExecutor countingExecutor = new CountingExecutor();
+		IdentifierToken identifier = new IdentifierToken("test", FileIndex.NULL_INDEX);
+		AssignToken assignToken = new AssignToken(FileIndex.NULL_INDEX);
+		assignToken.bind(identifier, new ValueToken(new NumberValue(13), FileIndex.NULL_INDEX));
+		LinkedList<Token> tokens = new LinkedList<>();
+		tokens.add(new ExecutableMockToken(countingExecutor));
+		tokens.add(new ExecutableMockToken(countingExecutor));
+		tokens.add(new ExecutableMockToken(countingExecutor));
+		tokens.add(assignToken); // This will not increment countingExecutor
+		BlockToken blockToken = new BlockToken(tokens, FileIndex.NULL_INDEX);
+		Scope globalScope = Scope.createGlobalScope(null);
+		blockToken.executeBlock(globalScope);
+		countingExecutor.assertCount(3);
+		assertThrows(RuntimeException.class, () -> globalScope.variableProvider.get(identifier));
+	}
+
+	@Test
+	public void testExecute_modifyParentVariable() throws RuntimeException, ParsingException {
+		IdentifierToken identifier = new IdentifierToken("test", FileIndex.NULL_INDEX);
+		AssignToken assignToken = new AssignToken(FileIndex.NULL_INDEX);
+		assignToken.bind(identifier, new ValueToken(new NumberValue(17), FileIndex.NULL_INDEX));
+		LinkedList<Token> tokens = new LinkedList<>();
+		tokens.add(assignToken);
+		BlockToken blockToken = new BlockToken(tokens, FileIndex.NULL_INDEX);
+		Scope globalScope = Scope.createGlobalScope(null);
+		globalScope.variableProvider.setLocal(identifier, new NumberValue(13));
+		blockToken.executeBlock(globalScope);
+		assertEquals(17, globalScope.variableProvider.get(identifier).getJavaValue());
+	}
 }
