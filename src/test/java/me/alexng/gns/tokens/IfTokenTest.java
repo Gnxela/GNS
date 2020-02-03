b73e6d28cec87ed6932859dc180a61698271414b
Gnxela <qawswedfrgtt@gmail.com>
2020-02-03 11:31:25 +0000

    Added tests for IfToken.

---
+++ src/test/java/me/alexng/gns/tokens/IfTokenTest.java
@@ -0,0 +1,46 @@
+package me.alexng.gns.tokens;
+
+import me.alexng.gns.FileIndex;
+import me.alexng.gns.RuntimeException;
+import me.alexng.gns.env.scope.Scope;
+import me.alexng.gns.env.value.BooleanValue;
+import me.alexng.gns.env.value.Value;
+import me.alexng.gns.util.CountingExecutor;
+import me.alexng.gns.util.ExecutableMockToken;
+import me.alexng.gns.util.ReturningExecutor;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IfTokenTest {
+
+	@Test
+	public void testExecute() throws RuntimeException {
+		ReturningExecutor returningExecutor = new ReturningExecutor(BooleanValue.TRUE);
+		ExpressionToken expressionToken = new ExpressionToken(new ExecutableMockToken(returningExecutor), FileIndex.NULL_INDEX);
+		CountingExecutor countingExecutor = new CountingExecutor();
+		LinkedList<Token> tokens = new LinkedList<>();
+		tokens.add(new ExecutableMockToken(countingExecutor));
+		tokens.add(new ExecutableMockToken(countingExecutor));
+		tokens.add(new ExecutableMockToken(countingExecutor));
+		BlockToken blockToken = new BlockToken(tokens, FileIndex.NULL_INDEX);
+		IfToken ifToken = new IfToken(expressionToken, blockToken, FileIndex.NULL_INDEX);
+
+		ifToken.execute(Scope.createGlobalScope(null));
+		countingExecutor.assertCount(3);
+
+		returningExecutor.setValue(BooleanValue.FALSE);
+		ifToken.execute(Scope.createGlobalScope(null));
+		countingExecutor.assertCount(3);
+	}
+
+	@Test
+	public void testExecute_throws() throws RuntimeException {
+		ExpressionToken expressionToken = new ExpressionToken(new ExecutableMockToken(new ReturningExecutor(Value.NULL)), FileIndex.NULL_INDEX);
+		BlockToken blockToken = new BlockToken(new LinkedList<>(), FileIndex.NULL_INDEX);
+		IfToken ifToken = new IfToken(expressionToken, blockToken, FileIndex.NULL_INDEX);
+		assertThrows(RuntimeException.class, () -> ifToken.execute(Scope.createGlobalScope(null)));
+	}
+}
---
+++ src/test/java/me/alexng/gns/util/ReturningExecutor.java
@@ -0,0 +1,25 @@
+package me.alexng.gns.util;
+
+import me.alexng.gns.env.value.Value;
+
+public class ReturningExecutor extends ExecutableMockToken.Executor {
+
+	private Value value;
+
+	public ReturningExecutor(Value value) {
+		this.value = value;
+	}
+
+	@Override
+	public Value execute() {
+		return value;
+	}
+
+	public Value getValue() {
+		return value;
+	}
+
+	public void setValue(Value value) {
+		this.value = value;
+	}
+}
