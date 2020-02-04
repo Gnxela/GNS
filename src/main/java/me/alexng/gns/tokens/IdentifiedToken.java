e2191369c873847789f2ad392dea0024c1e19e09
Gnxela <qawswedfrgtt@gmail.com>
2020-02-04 18:16:17 +0000

    Removed CharSequence from IdentifiedToken. I originally did this so I could either pass strings or Identifiers to methods, but I never ended up using this functionality. I would like to do this again in the future but it will require some refactoring.

--- src/main/java/me/alexng/gns/tokens/IdentifiedToken.java
+++ src/main/java/me/alexng/gns/tokens/IdentifiedToken.java
@@ -2,28 +2,13 @@ package me.alexng.gns.tokens;
 
 import me.alexng.gns.FileIndex;
 
-public abstract class IdentifiedToken extends Token implements CharSequence {
+public abstract class IdentifiedToken extends Token {
 
 	private IdentifierToken identifier;
 
 	public IdentifiedToken(IdentifierToken identifier, FileIndex fileIndex) {
 		super(fileIndex);
 		this.identifier = identifier;
-	}
-
-	@Override
-	public int length() {
-		return identifier.getName().length();
-	}
-
-	@Override
-	public char charAt(int i) {
-		return identifier.getName().charAt(i);
-	}
-
-	@Override
-	public CharSequence subSequence(int i, int i1) {
-		return identifier.getName().subSequence(i, i1);
 	}
 
 	public IdentifierToken getIdentifier() {
