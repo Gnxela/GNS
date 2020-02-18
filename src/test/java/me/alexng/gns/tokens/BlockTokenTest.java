de953527cc4ce030513237031496178aa150d0d7
Gnxela <qawswedfrgtt@gmail.com>
2020-02-18 18:05:58 +0000

    Added tests for BlockToken return values.

--- src/test/java/me/alexng/gns/tokens/BlockTokenTest.java
+++ src/test/java/me/alexng/gns/tokens/BlockTokenTest.java
@@ -5,15 +5,17 @@ import me.alexng.gns.ParsingException;
 import me.alexng.gns.RuntimeException;
 import me.alexng.gns.env.scope.Scope;
 import me.alexng.gns.env.value.NumberValue;
+import me.alexng.gns.env.value.ReturnedValue;
+import me.alexng.gns.env.value.Value;
 import me.alexng.gns.tokens.operators.AssignToken;
 import me.alexng.gns.util.CountingExecutor;
 import me.alexng.gns.util.ExecutableMockToken;
+import me.alexng.gns.util.ReturningMockToken;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BlockTokenTest {
 
@@ -58,6 +60,27 @@ public class BlockTokenTest {
 		Scope globalScope = Scope.createGlobalScope(null);
 		blockToken.executeBlock(globalScope);
 		countingExecutor.assertCount(3);
+		assertThrows(RuntimeException.class, () -> globalScope.variableProvider.get(identifier));
+	}
+
+	@Test
+	public void testExecute_returnValue() throws RuntimeException, ParsingException {
+		CountingExecutor countingExecutor = new CountingExecutor();
+		IdentifierToken identifier = new IdentifierToken("test", FileIndex.NULL_INDEX);
+		AssignToken assignToken = new AssignToken(FileIndex.NULL_INDEX);
+		assignToken.bind(identifier, new ValueToken(new NumberValue(13), FileIndex.NULL_INDEX));
+		LinkedList<Token> tokens = new LinkedList<>();
+		tokens.add(new ExecutableMockToken(countingExecutor));
+		tokens.add(new ExecutableMockToken(countingExecutor));
+		tokens.add(new ReturningMockToken(new ReturnedValue(new NumberValue(1))));
+		tokens.add(new ExecutableMockToken(countingExecutor)); // This token is not executed.
+		tokens.add(assignToken); // This will not increment countingExecutor
+		BlockToken blockToken = new BlockToken(tokens, FileIndex.NULL_INDEX);
+		Scope globalScope = Scope.createGlobalScope(null);
+		Value value = blockToken.executeBlock(globalScope);
+		countingExecutor.assertCount(2);
+		assertTrue(value instanceof ReturnedValue);
+		assertEquals(1, ((ReturnedValue) value).getJavaValue().getJavaValue());
 		assertThrows(RuntimeException.class, () -> globalScope.variableProvider.get(identifier));
 	}
 
