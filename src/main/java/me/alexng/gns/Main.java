05242995436fb20a28870f75beb95f97beb103e2
Gnxela <qawswedfrgtt@gmail.com>
2020-06-03 22:31:37 +0100

    Removed debug code.
    In the last commit I forgot I included the optimisations of ObjectToStringCrawler; so that too.

--- src/main/java/me/alexng/gns/Main.java
+++ src/main/java/me/alexng/gns/Main.java
@@ -2,9 +2,6 @@ package me.alexng.gns;
 
 import me.alexng.gns.env.Environment;
 import me.alexng.gns.env.Script;
-import me.alexng.gns.tokens.IdentifierToken;
-import me.alexng.gns.tokens.value.ObjectValue;
-import me.alexng.gns.util.ObjectToStringCrawler;
 
 import java.io.File;
 
@@ -22,17 +19,5 @@ public class Main {
 		long endTime = System.nanoTime();
 		System.out.println("Parse time: " + (endParse - start) / 1000000 + "ms");
 		System.out.println("Run time: " + (endTime - endParse) / 1000000 + "ms");
-
-		ObjectValue object = (ObjectValue) scriptEnvironment.getGlobalScope().getValue(new IdentifierToken("o3", FileIndex.INTERNAL_INDEX));
-		long length = 100000;
-		long total = 0;
-		for (int i = 0; i < length; i++) {
-			long s = System.nanoTime();
-			object.getObjectScope().crawl(new ObjectToStringCrawler());
-			total += System.nanoTime() - s;
-		}
-		System.out.println("Stringify: " + (total / length) / 1000000 + "ms");
-		System.out.println("Stringify: " + (total / length) / 1000 + "us");
-		System.out.println("Stringify: " + (total / length) + "ns");
 	}
 }
